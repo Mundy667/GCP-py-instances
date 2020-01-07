d70da3e215351d710a58d7446e0dcfb82823d8a9
Sven Mundschenk <svenmundschenk@svenmundschenk-macbookpro.roam.corp.google.com>
2020-01-07 17:55:21 +0100

    instance name must to be defined now, no default anymore

--- start-instances.py
+++ start-instances.py
@@ -15,15 +15,15 @@ def list_instances(compute, project, zone):
 def main(project, zone, instance_name, wait=True):
     credentials = GoogleCredentials.get_application_default()
     compute = discovery.build('compute', 'v1', credentials=credentials)
-    
 
     instances = list_instances(compute, project, zone)
 
-    print('Instances in project %s and zone %s:' % (project, zone))
+    print('Starting Instance %s in project %s and zone %s:' % (instance_name, project, zone))
+
     for instance in instances:
-        print(' - ' + instance['name'])
-
-
+        if instance['name'] == instance_name:
+            print(' - IP Adress of ' + instance['name'] + ' \n')
+            print(instance['networkInterfaces'][0]['accessConfigs'][0]['natIP'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -32,12 +32,10 @@ if __name__ == '__main__':
     parser.add_argument('project_id', help='Your Google Cloud project ID.')
     parser.add_argument(
         '--zone',
-        default='us-west3-c',
+        default='europe-west3-c',
         help='Compute Engine zone to work in.')
     parser.add_argument(
-        '--name', default='buba-internal-dev', help='New instance name.')
-
+        'name', help='the instance name you want to start.')
     args = parser.parse_args()
-
     main(args.project_id, args.zone, args.name)
 # [END run]
